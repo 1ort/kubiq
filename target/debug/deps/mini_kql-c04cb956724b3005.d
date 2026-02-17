6b875d9ae996a97455582c8deac1e731603fe446
1ort <83316072+1ort@users.noreply.github.com>
2026-02-17 20:24:02 +0500

    Ignore Cargo build artifacts

--- target/debug/deps/mini_kql-c04cb956724b3005.d
+++
@@ -1,5 +0,0 @@
-/home/dan/Documents/kql/target/debug/deps/mini_kql-c04cb956724b3005.d: src/main.rs
-
-/home/dan/Documents/kql/target/debug/deps/mini_kql-c04cb956724b3005: src/main.rs
-
-src/main.rs:
--- target/debug/deps/mini_kql-e75bd69285b2f635.d
+++
@@ -1,9 +0,0 @@
-/home/dan/Documents/kql/target/debug/deps/mini_kql-e75bd69285b2f635.d: src/main.rs Cargo.toml
-
-/home/dan/Documents/kql/target/debug/deps/libmini_kql-e75bd69285b2f635.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
